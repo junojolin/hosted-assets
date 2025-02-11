db62160e626ed1f364cd06a46fdb5aacf6aa1897
junojolin <125191264+junojolin@users.noreply.github.com>
2025-02-10 16:07:42 -1000

    Update contactScript.js

--- contactScript.js
+++ contactScript.js
@@ -1,16 +1,19 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycby-p9U4VawSk8QTnxOdTUVdtUAu28GBWpUJdDicfQ95qcpqMiU2c4fyLGZ9HnkHSZfX/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById("msg")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwMTRXClO2cE0cu6wqnUs0GNAXoU_7Cl6l9eB5DEFYdnPVM19ODu1ZPpULOIssOTNGJ/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-      msg.innerHTML = "Message sent succesfully!"
-      setTimeout(function(){
-        msg.innerHTML = ""
-      },5000)
-      form.reset()
-    })
-      .catch(error => console.error('Error!', error.message))
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  fetch(scriptURL, { 
+    method: 'POST', 
+    body: new FormData(form)
   })
+  .then(response => {
+    msg.innerHTML = "Message sent successfully!";
+    setTimeout(function() {
+      msg.innerHTML = "";
+    }, 5000);
+    form.reset();
+  })
+  .catch(error => console.error('Error!', error.message));
+});
